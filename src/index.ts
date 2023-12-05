aada257da84dc0b8045d112486b5db2c9aaa4b76
Marvin Frede <mfrede@slashwhy.de>
2023-12-05 13:36:04 +0100

    solve day 03 part 1

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 import { readFileSync } from "fs";
+import { Matrix } from "./matrix";
 
-const getLines = (filename: string) => {
+const getLines = (filename: string): string[] => {
   const file = readFileSync(filename, "utf-8");
   const lines = file.split("\n");
   return lines;
 };
 
 const main = () => {
-  const lines = getLines("./input.txt");
   let sum = 0;
-  lines.forEach((line) => {
-    if (line) {
-      sum += 0;
+  const matrix = new Matrix(getLines("./input.txt"));
+  let number = 0;
+  matrix.getMatrix().forEach((line, y) => {
+    for (let x = 0; x < line.length; x++) {
+      const char = line.charAt(x);
+      if (isNaN(+char)) {
+        if (number) {
+          if (
+            x === 0 // last number found was in previous line
+              ? matrix.isNextToSymbol(number, { x: line.length, y: y - 1 })
+              : matrix.isNextToSymbol(number, { x, y })
+          ) {
+            sum += number;
+          }
+          number = 0;
+        }
+        continue;
+      }
+      number = +`${number}${char}`;
     }
   });
   console.log("sum: ", sum);
---
+++ src/matrix.ts
@@ -0,0 +1,73 @@
+export interface Coord {
+  x: number;
+  y: number;
+}
+
+export class Matrix {
+  private lines: string[];
+
+  public constructor(lines: string[]) {
+    this.lines = lines;
+  }
+
+  public getMatrix() {
+    return this.lines;
+  }
+
+  public get(pos: Coord): string {
+    if (!this.isCoordValid(pos)) {
+      console.trace("pos: ", pos);
+      throw new Error("Out of Bounds!");
+    }
+    const { x, y } = pos;
+    return this.lines[y].charAt(x);
+  }
+
+  public isSymbol(pos: Coord) {
+    return this.get(pos).match(/\d|\./) === null;
+  }
+
+  public getSurroundingPositions(pos: Coord): Coord[] {
+    const { x, y } = pos;
+    const above = [
+      { x: x - 1, y: y - 1 },
+      { x: x, y: y - 1 },
+      { x: x + 1, y: y - 1 },
+    ];
+    const leftRight = [
+      { x: x - 1, y: y },
+      { x: x + 1, y: y },
+    ];
+    const below = [
+      { x: x - 1, y: y + 1 },
+      { x: x, y: y + 1 },
+      { x: x + 1, y: y + 1 },
+    ];
+    return [...above, ...leftRight, ...below].filter((pos) => this.isCoordValid(pos));
+  }
+
+  public isNextToSymbol(number: number, posInSameLineAfterNumber: Coord) {
+    const { x: posX, y: posY } = posInSameLineAfterNumber;
+    const len = `${number}`.length;
+    for (let x = posX - len; x < posX; x++) {
+      const surroundingPositions = this.getSurroundingPositions({ x, y: posY });
+      for (let index = 0; index < surroundingPositions.length; index++) {
+        const pos = surroundingPositions[index];
+        if (this.isSymbol(pos)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  private isCoordValid(pos: Coord): boolean {
+    if (pos.y < 0 || pos.y >= this.lines.length) {
+      return false;
+    }
+    if (pos.x < 0 || pos.x >= this.lines[pos.y].length) {
+      return false;
+    }
+    return true;
+  }
+}
