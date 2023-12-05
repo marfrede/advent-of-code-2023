6fb8d1407fa81e6a88a1a556aad5523670598d25
Marvin Frede <mfrede@slashwhy.de>
2023-12-05 15:50:28 +0100

    filter empty lines instantly

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,14 @@ import { readFileSync } from "fs";
 const getLines = (filename: string) => {
   const file = readFileSync(filename, "utf-8");
   const lines = file.split("\n");
-  return lines;
+  return lines.filter((l) => !!l);
 };
 
 const main = () => {
   const lines = getLines("./input.txt");
   let sum = 0;
   lines.forEach((line) => {
-    if (line) {
-      sum += 0;
-    }
+    sum += 0;
   });
   console.log("sum: ", sum);
 };
