880f799bbd4f11d6d147b2bfd2f8f71a1cd2aef9
Marvin Frede <mfrede@slashwhy.de>
2023-12-08 16:59:00 +0100

    solve day 06 part 1 & 2

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,33 @@ const getLines = (filename: string) => {
   return lines.filter((l) => !!l);
 };
 
+const interpretLines = (lines: string[]) => {
+  const tRx = /Time:\s+/;
+  const dRx = /Distance:\s+/;
+  const times = lines[0].replace(tRx, "").split(/\s+/).map(Number);
+  const distances = lines[1].replace(dRx, "").split(/\s+/).map(Number);
+  return { times, distances };
+};
+
+const countWaysToBeatRecord = (time: number, distance: number): number => {
+  let sum = 0;
+  for (let timePressed = 0; timePressed < time; timePressed++) {
+    const distanceTraveled = (time - timePressed) * timePressed;
+    if (distanceTraveled > distance) sum++;
+  }
+  return sum;
+};
+
 const main = () => {
   const lines = getLines("./input.txt");
-  let sum = 0;
-  lines.forEach((line) => {
-    sum += 0;
-  });
-  console.log("sum: ", sum);
+  const { times, distances } = interpretLines(lines);
+  let product = 1;
+  for (let i = 0; i < times.length; i++) {
+    const time = times[i];
+    const distanceRecord = distances[i];
+    product = product * countWaysToBeatRecord(time, distanceRecord);
+  }
+  console.log("product: ", product);
 };
 
 main();
