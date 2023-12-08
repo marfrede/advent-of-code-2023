340a51d8baacbeff2c4bec1b7538f688aa7b2283
Marvin Frede <mfrede@slashwhy.de>
2023-12-08 17:14:08 +0100

    Revert "approach so far..."
    This reverts commit d703bc65abc93798a62621e7a68992c165da7c8b.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { appendFileSync, readFileSync, writeFileSync } from "fs";
-import { Hand } from "./camel-card-hand.class";
+import { readFileSync } from "fs";
 
 const getLines = (filename: string) => {
   const file = readFileSync(filename, "utf-8");
@@ -9,25 +8,10 @@ const getLines = (filename: string) => {
 
 const main = () => {
   const lines = getLines("./input.txt");
-
-  const hands: Hand[] = [];
-  lines.forEach((line) => {
-    const lineMatch = line.match(/(.{5})\s+(\d+)/);
-    hands.push(new Hand(lineMatch!.at(1) as string, +(lineMatch!.at(2) as string)));
-  });
-
-  writeFileSync("./output.json", "[");
-  hands
-    .toSorted((a, b) => a.isStronger(b))
-    .forEach((h, i) => appendFileSync("./output.json", JSON.stringify(h) + ",\n\n"));
-  appendFileSync("./output.json", "]");
-
   let sum = 0;
-  hands
-    .toSorted((a, b) => a.isStronger(b))
-    .forEach((hand, i) => {
-      sum += hand.bid * (i + 1);
-    });
+  lines.forEach((line) => {
+    sum += 0;
+  });
   console.log("sum: ", sum);
 };
 
