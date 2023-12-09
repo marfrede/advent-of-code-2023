7a4e9724cc1b1a80ab921f48f5a8558a632bc5fa
Marvin Frede <mfrede@slashwhy.de>
2023-12-09 15:35:40 +0100

    solve day 09 part 1

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,49 @@ const getLines = (filename: string) => {
   return lines.filter((l: string) => !!l);
 };
 
-const findNextElem = (sequence: number[]) => {};
+const elementsAreAllTheSame = (array: number[]): boolean => {
+  return array.every((num) => num === array[0]);
+};
+
+const createDiffArrays = (sequence: number[]): number[][] => {
+  const diffArrays: number[][] = [sequence];
+  while (!elementsAreAllTheSame(diffArrays.last())) {
+    diffArrays.push([]);
+    const seq = diffArrays.secondLast();
+    for (let i = 0; i < seq.length; i++) {
+      const num: number = seq[i];
+      const nextNum: number | undefined = seq.at(i + 1);
+      if (nextNum !== undefined) {
+        diffArrays.last().push(nextNum - num);
+      }
+    }
+  }
+  return diffArrays;
+};
+
+const addNextElemToDiffArrays = (diffArrays: number[][]) => {
+  let toAdd = diffArrays.last()[0];
+  for (let i = diffArrays.length - 2; i >= 0; i--) {
+    const diffArray = diffArrays[i];
+    toAdd = diffArray.last() + toAdd;
+    diffArray.push(toAdd);
+  }
+  return diffArrays;
+};
+
+const findNextElem = (sequence: number[]) => {
+  const diffArrays = createDiffArrays(sequence);
+  addNextElemToDiffArrays(diffArrays);
+  return diffArrays[0].last();
+};
 
 const main = () => {
   const lines = getLines("./input.txt");
 
-  const sum = lines.reduce((prevSum, line) => {
-    return prevSum + 1;
+  const sum = lines.reduce((prevSum, line, i) => {
     const sequence: number[] = line.split(" ").map(Number);
+    const nextElem = findNextElem(sequence);
+    return prevSum + nextElem;
   }, 0);
   console.log("sum: ", sum);
 };
