ed5bca737c825216dc1432b7e6e935b4fe745b26
Marvin Frede <mfrede@slashwhy.de>
2023-12-11 07:21:03 +0100

    solve day 11 part 1

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,73 @@
 import { readFileSync } from "fs";
 
+type Coord = { x: number; y: number };
+
+const GAL = "#";
+const xsWithoutGalaxy: number[] = [];
+const ysWithoutGalaxy: number[] = [];
+const galaxies: Coord[] = [];
+
 const getLines = (filename: string) => {
   const file = readFileSync(filename, "utf-8");
   const lines = file.split("\n");
   return lines.filter((l) => !!l);
 };
 
+const readXsWithoutGalaxy = (universe: string[]) => {
+  for (let x = 0; x < universe[0].length; x++) {
+    let column: string = "";
+    for (let y = 0; y < universe.length; y++) {
+      const line: string = universe[y];
+      const char = line[x];
+      if (char === GAL) {
+        galaxies.push({ x, y });
+      }
+      column += char;
+    }
+    if (!column.includes(GAL)) xsWithoutGalaxy.push(x);
+  }
+  return xsWithoutGalaxy;
+};
+
+const readYsWithoutGalaxy = (universe: string[]) => {
+  for (let y = 0; y < universe.length; y++) {
+    const line: string = universe[y];
+    if (!line.includes("#")) ysWithoutGalaxy.push(y);
+  }
+  return ysWithoutGalaxy;
+};
+
+const calcDistance = (aGal: Coord, bGal: Coord) => {
+  const minX = Math.min(aGal.x, bGal.x);
+  const maxX = Math.max(aGal.x, bGal.x);
+  const minY = Math.min(aGal.y, bGal.y);
+  const maxY = Math.max(aGal.y, bGal.y);
+  const distNoExpansion = maxX - minX + (maxY - minY);
+  let dist = distNoExpansion;
+  for (const xWithout of xsWithoutGalaxy) {
+    if (minX < xWithout && maxX > xWithout) dist = dist + 1;
+  }
+  for (const yWithout of ysWithoutGalaxy) {
+    if (minY < yWithout && maxY > yWithout) dist = dist + 1;
+  }
+  return dist;
+};
+
+const calcDistances = () => {
+  let distanceSum = 0;
+  for (let i = 0; i < galaxies.length; i++) {
+    for (let j = 0; j < galaxies.length; j++) {
+      if (i !== j && i > j) distanceSum += calcDistance(galaxies[i], galaxies[j]);
+    }
+  }
+  return distanceSum;
+};
+
 const main = () => {
   const lines = getLines("./input.txt");
-  let sum = 0;
-  lines.forEach((line) => {
-    sum += 0;
-  });
+  readYsWithoutGalaxy(lines);
+  readXsWithoutGalaxy(lines);
+  const sum = calcDistances();
   console.log("sum: ", sum);
 };
 
