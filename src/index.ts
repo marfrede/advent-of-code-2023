0c85047ab56ef9799973194f04664439a912fc6a
Marvin Frede <mfrede@slashwhy.de>
2023-12-14 06:39:37 +0100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -22,26 +22,18 @@ const convertToColumns = (lines: string[]) => {
 };
 
 const getValueOfCol = (col: string) => {
-  let value = 0;
   const splittedAfterRocks = (col.match(/[^#]+#?|#/g) || []).map((s) => s ?? "");
-  // const len = strings.reduce((sum, st) => sum + st.length, 0);
-
+  let value = 0;
   let indexPointsLeft = COL_LEN;
   for (const lineBeforeRock of splittedAfterRocks) {
     if (lineBeforeRock.includes("O")) {
       const numberOfOs = (lineBeforeRock.match(/O/g) || []).length;
-      console.log("\t\tstr: ", lineBeforeRock);
-      console.log("\t\tnumberOfOs: ", numberOfOs);
       for (let i = 0; i < numberOfOs; i++) {
         value += indexPointsLeft - i;
-        console.log("\t\t\tpoints: ", indexPointsLeft - i);
       }
     }
     indexPointsLeft = indexPointsLeft - lineBeforeRock.length;
   }
-  console.log("\tstrings: ", splittedAfterRocks);
-  console.log("\tpoints: ", value);
-  // console.log("\tlen: ", len);
   return value;
 };
 
@@ -49,10 +41,8 @@ const main = () => {
   const lines: string[] = getLines("./input.txt");
   const columns: string[] = convertToColumns(lines);
   COL_LEN = columns[0].length;
-  const sum = columns.reduce((prevSum, col) => {
-    console.log("col: ", col);
-    return prevSum + getValueOfCol(col);
-  }, 0);
+
+  const sum = columns.reduce((prevSum, col) => prevSum + getValueOfCol(col), 0);
   console.log("sum: ", sum);
 };
 
